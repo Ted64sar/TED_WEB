f968c6aeca18ec5d14d19cebb202a9a6bbcdb403
Smart Ted <fedor-commander@yandex.ru>
2019-03-01 16:53:04 +0400

    пар-ла-ла

---
+++ IMG.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    '''<img src="{}" alt="здесь должна была быть картинка, 
+        но не нашлась">'''.format(url_for('static', filename='img/Риана.jpg'))
+
+    return "Привет, Яндекс!"
+
+@app.route('/')
+@app.route('/image_sample')
+def image():
+    return '''<img src="/static/img/O.jpeg" alt="здесь у нас многострадальная Риана" >'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
--- server.py
+++ server.py
@@ -1,5 +1,18 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
-
+from flask import Flask
 server_address = ("", 8000)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Привет, Яндекс!"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
